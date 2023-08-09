feadbe2d62f03f2c7c10ecaebf4ba88d97906e16
Oulinstar <terleeva89@gmail.com>
2023-08-09 14:54:04 +0200

    add login/regester/logout

---
+++ src/Controller/UserController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Todo;
+use App\Repository\TodoRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/user')]
+class UserController extends AbstractController
+{
+    #[Route('/', name: 'app_user')]
+    public function index(TodoRepository $todoRepository): Response
+    {
+        $user = $this->getUser();
+        return $this->render('user/index.html.twig', [
+            'todos' => $todoRepository->findAll(),
+            'user' => $user
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_todo_show', methods: ['GET'])]
+    public function show(Todo $todo): Response
+    {
+        return $this->render('user/details.html.twig', [
+            'todo' => $todo,
+        ]);
+    }
+}
